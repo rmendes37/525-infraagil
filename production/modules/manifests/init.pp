6586e70c4fd25515f54eae2d8ef43b6068e393b5
Revair <rmendes37@gmail.com>
2018-10-03 20:14:54 +0000

    Adicionando  puppet

---
+++ production/modules/manifests/init.pp
@@ -0,0 +1,6 @@
+class web{
+
+package
+
+}
+
---
+++ puppet/init.pp
@@ -0,0 +1,16 @@
+class base {
+case $::osfamily{
+
+"RedHat":{
+$pacotes = ["epel-release", "git", "vim", "sysstat", "htop", "httpd", "figlet"]
+}
+
+"Debian":{
+        exec{"atualiza_pacotes":
+             command => "/usr/bin/apt update"
+         }
+
+$pacotes=["git","vim","sysstat","http","cowsay","figlet"]
+}
+}
+
